ccfda70d6b8fd76d3dd9981bea6446490677a88c
KeanuRayhan <keanu.rayhan.tif422@polban.ac.id>
2025-04-29 08:52:59 +0700

    Add CalculatorTest

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,20 @@
+import com.javakeanu.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    void testTambah() {
+        // Arrange (siapkan input)
+        int a = 5;
+        int b = 3;
+
+        // Act (panggil method yang mau dites)
+        int result = Calculator.tambah(a, b);
+
+        // Assert (cek hasilnya)
+        assertEquals(8, result, "5 + 3 seharusnya 8");
+    }
+}
