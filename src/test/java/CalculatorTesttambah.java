4e3cadf2c37d104dcc2aa84ce05a9afbde0e3905
RezaMaulana <reza.maulana.tif422@polban.ac.id>
2025-05-04 16:11:36 +0700

    update test module tambah Reza

--- src/test/java/CalculatorTesttambah.java
+++ src/test/java/CalculatorTesttambah.java
@@ -2,36 +2,35 @@ import com.javakeanu.Computation;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CalculatorTesttambah {
+class CalculatorTesttambah {
 
     @Test
-    public void TC1_inputDuaAngkaPositif_tidakThrowException() {
+    void TC1_inputDuaAngkaPositif_tidakThrowException() {
         assertDoesNotThrow(() -> {
             int input1 = Computation.parseInput("300");
             int input2 = Computation.parseInput("578");
-            // Misalnya operasi penjumlahan
             int hasil = input1 + input2;
-            assertEquals(878, hasil); // Optional: cek hasil
+            assertEquals(878, hasil);
         });
     }
 
     @Test
-    public void TC2_inputDuaAngkaNegatif_tidakThrowException() {
+    void TC2_inputDuaAngkaNegatif_tidakThrowException() {
         assertDoesNotThrow(() -> {
             int input1 = Computation.parseInput("-3200");
             int input2 = Computation.parseInput("-340");
             int hasil = input1 + input2;
-            assertEquals(-3540, hasil); // Optional
+            assertEquals(-3540, hasil);
         });
     }
 
     @Test
-    public void TC3_inputSatuPositifSatuNegatif_tidakThrowException() {
+    void TC3_inputSatuPositifSatuNegatif_tidakThrowException() {
         assertDoesNotThrow(() -> {
             int input1 = Computation.parseInput("24560");
             int input2 = Computation.parseInput("-987");
             int hasil = input1 + input2;
-            assertEquals(23573, hasil); // Optional
+            assertEquals(23573, hasil);
         });
     }
 }
